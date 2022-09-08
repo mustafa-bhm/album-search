fec20d3c2613f0c85492d2829e27eda0535cce02
mustafa <mustafa.bhm3@gmail.com>
2022-09-08 14:23:26 -0600

    adding useEffect hook

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
@@ -7,13 +8,21 @@ export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    const url = ` https://search-itunes.vercel.app/?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`;
+    axios.get(url).then((response) => {
+      console.log("+++++", response.data.results);
+      setResults([...response.data.results]);
+    });
+  }, [term]);
+
   return (
     <Fragment>
       <header className="logo">
         <img src="images/brand.png" alt="Brand" />
       </header>
       <main>
-        <SearchBar onSearch={term => setTerm(term)} />
+        <SearchBar onSearch={(term) => setTerm(term)} />
         <Results results={results} />
       </main>
     </Fragment>
